6bec3ed4501e9f4b1cc67c9fc359916f51cda464
Nicole Thurnau <nthurnau@gmail.com>
2016-01-31 11:24:30 -0800

    added more variables to the app page

---
+++ app.js
@@ -0,0 +1,7 @@
+var answerBox = document.querySelector(".answer-input") //may not need
+var questionBox = document.getElementById("question-space")
+var questionCard = document.querySelectorAll(".question-top")
+var questionCard1 = document.getElementById("q1")
+var questionCard2 = document.getElementById("q2")
+var questionCard3 = document.getElementById("q3")
+var questionCard4 = document.getElementById("q4")
