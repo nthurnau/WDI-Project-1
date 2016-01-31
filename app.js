68ba2325f49b3ca4830db7803a6160c328870895
Nicole Thurnau <nthurnau@gmail.com>
2016-01-31 15:33:16 -0800

    finished questions, have eight

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var questionCard2 = document.getElementById("q2")
 var questionCard3 = document.getElementById("q3")
 var questionCard4 = document.getElementById("q4")
 
-
+//questions
 function askQuestion() {
   console.log(this.question)
 }
@@ -19,12 +19,37 @@ var questionOne = {question: "What is the head of Slytherin House's full name?",
 questionOne.askQuestion()
 questionOne.answerQuestion()
 
-var questionTwo = {question: "What is the head of Slytherin House's full name?",
-                   answer: "Severus Snape"}
+var questionTwo = {question: "How does Voldemort appear in Harry Potter and the Chamber of Secrets?",
+                   answer: "He appears via Tom Riddle who comes out of Ginny Weasely's diary."}
 questionTwo.askQuestion()
 questionTwo.answerQuestion()
 
-var questionThree = {question: "What is the head of Slytherin House's full name?",
-                   answer: "Severus Snape"}
+var questionThree = {question: "Who told Voldemort where Harry's parents were hiding?",
+                   answer: "Peter Pettigrew"}
 questionThree.askQuestion()
 questionThree.answerQuestion()
+
+var questionFour = {question: "What is Professor McGonnagal's animagus?",
+                   answer: "A tabby cat with white circles over its eyes"}
+questionFour.askQuestion()
+questionFour.answerQuestion()
+
+var questionFive = {question: "What are owls used for in the Wizarding world?",
+                   answer: "They carry, deliver, and pick up mail."}
+questionFive.askQuestion()
+questionFive.answerQuestion()
+
+var questionSix = {question: "What does S.P.E.W mean?",
+                   answer: "Society for the Promotion of Elifish Welfare"}
+questionSix.askQuestion()
+questionSix.answerQuestion()
+
+var questionSeven = {question: "At what age does a young witch or wizard get to go to Hogwarts?",
+                   answer: "11 years old"}
+questionSeven.askQuestion()
+questionSeven.answerQuestion()
+
+var questionEight = {question: "At what age does a young witch or wizard get to go to Hogwarts?",
+                   answer: "11 years old"}
+questionEight.askQuestion()
+questionEight.answerQuestion()
