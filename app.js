4499051c2fb71e3b9ea1a9241442876798cb3d55
Nicole Thurnau <nthurnau@gmail.com>
2016-01-31 14:18:25 -0800

    writing questions for game

--- app.js
+++ app.js
@@ -5,3 +5,26 @@ var questionCard1 = document.getElementById("q1")
 var questionCard2 = document.getElementById("q2")
 var questionCard3 = document.getElementById("q3")
 var questionCard4 = document.getElementById("q4")
+
+
+function askQuestion() {
+  console.log(this.question)
+}
+function answerQuestion() {
+  console.log(this.question)
+}
+
+var questionOne = {question: "What is the head of Slytherin House's full name?",
+                   answer: "Severus Snape"}
+questionOne.askQuestion()
+questionOne.answerQuestion()
+
+var questionTwo = {question: "What is the head of Slytherin House's full name?",
+                   answer: "Severus Snape"}
+questionTwo.askQuestion()
+questionTwo.answerQuestion()
+
+var questionThree = {question: "What is the head of Slytherin House's full name?",
+                   answer: "Severus Snape"}
+questionThree.askQuestion()
+questionThree.answerQuestion()
