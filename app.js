180d0b00b8676af8e0d7499fe3b25447871f712f
Nicole Thurnau <nthurnau@gmail.com>
2016-02-01 15:36:30 -0800

    revamped questions w new structure

--- app.js
+++ app.js
@@ -8,6 +8,82 @@ var questionCard4 = document.getElementById("q4")
 var turnCount = 0
 
 //questions
+
+var game = {
+  player1:{}
+  player2:{}
+  houses:{
+    gryffindor: {
+      currentQuestion = 0
+      name: "Gryffindor"
+      questions: [
+        {body: "What is the head of Slytherin House's full name?",
+        askQuestion: askQuestion,
+        answer: "d. Severus Snape",
+        answerOptions: ["a. Sibilus Snop", "b. Sirius Snape", "c. Snivelus Snape", "d. Severus Snape"],
+        answerQuestion: answerQuestion},
+
+        {body: "How does Voldemort appear in Harry Potter and the Chamber of Secrets?",
+        askQuestion: askQuestion,
+        answer: "c. As an apparition of young Tom Riddle",
+        answerOptions: ["a. As Himself, duh", "b. He's not in that one.", "c. As an apparition of young Tom Riddle", "d. As an apparition of middle-aged Tom Riddle",],
+        answerQuestion: answerQuestion}
+      ]
+    }
+    ravenclaw: {
+      currentQuestion = 0
+      name: "Ravenclaw"
+      questions: [
+        {body: "Who told Voldemort where Harry's parents were hiding?",
+        askQuestion: askQuestion,
+        answer: "a. Peter Pettigrew",
+        answerOptions: ["a. Peter Pettigrew", "b. Sirius Black", "c. Hargird", "d. Lucius Malfoy"],
+        answerQuestion: answerQuestion},
+
+        {body: "What is Professor McGonnagal's animagus?",
+        askQuestion: askQuestion,
+        answer: "d. A regular tabby with white circles on its eyes",
+        answerOptions: ["a. A black cat with white paws.", "b. An orange tabby with squares around its eyes", "c. A gray cat with white circles around its eyes", "d. A regular tabby with white circles on its eyes"],
+        answerQuestion: answerQuestion}
+      ]
+    }
+    slytherin: {
+      currentQuestion = 0
+      name: "Slytherin"
+      questions: [
+        {body: "What are owls used for in the Wizarding world?",
+        askQuestion: askQuestion,
+        answer: "c. A regular tabby with white circles on its eyes",
+        answerOptions: ["a. To lookout for enemies", "b. To be companion animals", "c. To send and deliver mail and packages", "d. To catch mice in one's home or farm"],
+        answerQuestion: answerQuestion},
+
+        {body: "What does S.P.E.W mean?",
+        askQuestion: askQuestion,
+        answer: "a. A regular tabby with white circles on its eyes",
+        answerOptions: ["a. Society for the Promotion of Elifish Welfare", "b. Society for the Prevention of Evil Warlocks", "c. Society for the Perversion of Entish Woodlands", "d. Society of the Pleasntness of Elastic Waistbands"],
+        answerQuestion: answerQuestion}
+      ]
+    }
+    hufflepuff: {
+      currentQuestion = 0
+      name: "hufflepuff"
+      questions: [
+        {body: "At what age does a young witch or wizard get to go to Hogwarts?",
+        askQuestion: askQuestion,
+        answer: "d. 11 years old",
+        answerOptions: ["a. 17 years old", "b. 5 years old", "c. 14 years old", "d. 11 years old"],
+        answerQuestion: answerQuestion},
+
+        {body: "What is the name of the town where Harry's parents hid and where Dumbledor grew up?",
+        askQuestion: askQuestion,
+        answer: "b. Godrick's Hollow",
+        answerOptions: ["a. Godrick's Fuller", "b. Godrick's Hollow", "c. Goodman's Hollow", "d. Little Whinging"],
+        answerQuestion: answerQuestion}
+    }
+  }
+
+}
+
 function askQuestion() {
   return this.question
 }
@@ -15,59 +91,7 @@ function answerQuestion() {
   return this.answer
 }
 
-var questionOne = {question: "What is the head of Slytherin House's full name?",
-                   askQuestion: askQuestion,
-                   answer: "Severus Snape",
-                   answerQuestion: answerQuestion}
-questionOne.askQuestion()
-questionOne.answerQuestion()
 
-var questionTwo = {question: "How does Voldemort appear in Harry Potter and the Chamber of Secrets?",
-                   askQuestion: askQuestion,
-                   answer: "He appears via Tom Riddle who comes out of Ginny Weasely's diary.",
-                   answerQuestion: answerQuestion}
-questionTwo.askQuestion()
-questionTwo.answerQuestion()
-
-var questionThree = {question: "Who told Voldemort where Harry's parents were hiding?",
-                   askQuestion: askQuestion,
-                   answer: "Peter Pettigrew",
-                   answerQuestion: answerQuestion}
-questionThree.askQuestion()
-questionThree.answerQuestion()
-
-var questionFour = {question: "What is Professor McGonnagal's animagus?",
-                   askQuestion: askQuestion,
-                   answer: "A tabby cat with white circles over its eyes",
-                   answerQuestion: answerQuestion}
-questionFour.askQuestion()
-questionFour.answerQuestion()
-
-var questionFive = {question: "What are owls used for in the Wizarding world?",
-                   askQuestion: askQuestion,
-                   answer: "They carry, deliver, and pick up mail.",
-                   answerQuestion: answerQuestion}
-questionFive.askQuestion()
-questionFive.answerQuestion()
-
-var questionSix = {question: "What does S.P.E.W mean?",
-                   askQuestion: askQuestion,
-                   answer: "Society for the Promotion of Elifish Welfare",
-                   answerQuestion: answerQuestion}
-questionSix.askQuestion()
-questionSix.answerQuestion()
-
-var questionSeven = {question: "At what age does a young witch or wizard get to go to Hogwarts?",
-                   askQuestion: askQuestion,
-                   answer: "11 years old",
-                   answerQuestion: answerQuestion}
-questionSeven.askQuestion()
-questionSeven.answerQuestion()
-
-var questionEight = {question: "What is the name of the town where Harry's parents hid and where Dumbledor grew up?",
-                   askQuestion: askQuestion,
-                   answer: "Godrick's Hollow",
-                   answerQuestion: answerQuestion}
 questionEight.askQuestion()
 questionEight.answerQuestion()
 
