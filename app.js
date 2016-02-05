4ee1678f598ac23ae4957325e2cef1243fc0792c
Nicole Thurnau <nthurnau@gmail.com>
2016-02-04 16:27:04 -0800

    finishing touches on styling

--- app.js
+++ app.js
@@ -3,17 +3,17 @@ var turnCount = 0
 var currentQuestion = 0 //tracks question number
 var player1Points = 0
 var player2Points = 0
-var showQuestionTime = 600 // 6000
-var answerWaitTime = 600 // 11000
-var answerDisplayTime = 3000 // 10000
-var fadeTime = 200 // 2000
+var showQuestionTime = 600 // change to 6000ms
+var answerWaitTime = 600 // change to 11000ms
+var answerDisplayTime = 9000 // change to 9000ms
+var fadeTime = 200 // change to 2000ms
 var questionId = null
 var correctAnswer = null
 var userAnswer = null
 var restartButton = $(".reset-button")
 
 
-//questions
+//questions function for questions in array below: gets called in there; same with answers
 function askQuestion() {
   return this.question
 }
@@ -22,7 +22,7 @@ function answerQuestion() {
 }
 restartButton.on("click", function() {window.location.reload()})
 
-
+//this allows the whole game to be a collection.
 var game = {
   selections: [], //array for player choices
   playerOne: {},
@@ -32,7 +32,6 @@ var game = {
 
 
   //starts the game on player one and their set of questions and answers. Also calls switchPlayerCheck at the right time
-
   gamePlay: function(){
     //game.currentPlayer = game.playerOne
     $(".answer-box-question").hide()
@@ -50,19 +49,20 @@ var game = {
     })
 
 
-
+//when the answer choice is clicked, it creates an array for the userAnswer, which helps with checkAnswer later
     $(".button").on("click", function(){
       userAnswer = $(this).text()
+      // $(".wandSound").play()
       console.log(userAnswer)
       game.checkAnswer() //calls checkAnswer!!
       game.switchPlayerCheck()//checks whose turn it is
-      function resetQuestions() {
+      function resetQuestions() { //resets the questions when it's player 2's turn
         if ($("#gryffindor").hasClass("unavailable") && $("#ravenclaw").hasClass("unavailable") && $("#slytherin").hasClass("unavailable") && $("#hufflepuff").hasClass("unavailable")){
           $(".question-top").removeClass("unavailable")
-          if (game.isGameOver()) {
-            game.checkWinner()
+          if (game.isGameOver()) { //runs a boolean to see if the game is over (8 questions is the max)
+            game.checkWinner() //announces the winner
           } else {
-            window.alert("It's Player Two's Turn!")
+            $(".answer-box-question").delay(8000).fadeIn(fadeTime).text("It's Player Two's Turn").delay(showQuestionTime).fadeOut(fadeTime)
           }
         }
       }
@@ -71,35 +71,30 @@ var game = {
   },
 
   checkAnswer: function() {
-    console.log("checking answers")
-    // console.log(correctAnswer)
-      if ((userAnswer === correctAnswer) && game.currentPlayer === 0) {
+      if ((userAnswer === correctAnswer) && game.currentPlayer === 0) { //checks the answer which was collected when answer btn clicked
             player1Points += 1
-            console.log(player1Points)
             $("#p1-score").html(player1Points)
             game.rightAnswerAnimation()
       } else if ((userAnswer === correctAnswer) && game.currentPlayer === 1) {
             player2Points += 1
-            console.log(player2Points)
             $("#p2-score").html(player2Points)
             game.rightAnswerAnimation()
           } else {
             game.wrongAnswerAnimation()
-            console.log("you're wrong!")
           }
     },
-
+//shows the answer choices after the questions appear for each house. They actually correspond
   displayAnswers: function(houseName) {
     game.displayAnswer("#a1", 0, houseName, game.currentPlayer)
     game.displayAnswer("#a2", 1, houseName, game.currentPlayer)
     game.displayAnswer("#a3", 2, houseName, game.currentPlayer)
     game.displayAnswer("#a4", 3, houseName, game.currentPlayer)
   },
-
+//controls the animation for the buttons
   displayAnswer: function(buttonId, answerIndex, houseName){
     $(buttonId).delay(answerWaitTime).fadeIn(fadeTime).text(game.houses[questionId].questions[game.currentPlayer].answerOptions[answerIndex]).delay(answerDisplayTime).fadeOut(fadeTime).hide(".button")
   },
-
+//does exactly what it says!
   switchPlayerCheck: function(){
     if (currentQuestion < 4) {
       game.currentPlayer = 0
@@ -112,27 +107,28 @@ var game = {
     console.log("currentQuestion:", currentQuestion)
   },
 
+//shows a gif when player answer right and when a player answers wrong
   rightAnswerAnimation: function() {
-    $("#right").fadeIn(2000).delay(1000).fadeOut(2000).hide("#right")
+    $("#right").fadeIn(2000).delay(1000).fadeOut(1000).hide("#right")
   },
   wrongAnswerAnimation: function() {
-    $("#wrong").fadeIn(2000).delay(1000).fadeOut(2000).hide("#wrong")
+    $("#wrong").fadeIn(2000).delay(1000).fadeOut(1000).hide("#wrong")
   },
-
+//controls the animation on the questions
   displayQuestion: function(houseName) {
      $('#question-space').fadeIn(fadeTime).text(game.houses[questionId].questions[game.currentPlayer].body).delay(showQuestionTime).fadeOut(fadeTime).hide(".answer-box-question")
   },
-//checks to see who wins
+//checks to see who wins and shows an animation on the question box
  checkWinner: function() {
    if (player1Points < player2Points) {
-     $(".answer-box-question").fadeIn(fadeTime).text("Player Two Wins!").delay(showQuestionTime).fadeOut(fadeTime) &&
-     $("#win").delay(2000).fadeIn(fadeTime).fadeOut(fadeTime)
+     $(".answer-box-question").delay(8000).fadeIn(fadeTime).text("Player Two Wins!").delay(showQuestionTime).fadeOut(fadeTime) &&
+     $("#win").delay(8000).fadeIn(3000)
    } else if (player1Points == player2Points) {
-     $(".answer-box-question").fadeIn(fadeTime).text("It's a Tie!").delay(showQuestionTime).fadeOut(fadeTime) &&
-      $("#win").delay(2000).fadeIn(fadeTime).fadeOut(fadeTime)
+     $(".answer-box-question").delay(8000).delay().fadeIn(fadeTime).text("It's a Tie!").delay(showQuestionTime).fadeOut(fadeTime) &&
+      $("#win").delay(8000).fadeIn(3000)
    } else {
-     $(".answer-box-question").fadeIn(fadeTime).text("Player One Wins!").delay(showQuestionTime).fadeOut(fadeTime) &&
-      $("#win").delay(5000).fadeIn(fadeTime).fadeOut(fadeTime)
+     $(".answer-box-question").delay(8000).fadeIn(fadeTime).text("Player One Wins!").delay(showQuestionTime).fadeOut(fadeTime) &&
+      $("#win").delay(8000).fadeIn(3000)
    }
 
  },
@@ -140,7 +136,7 @@ var game = {
   isGameOver: function() {
    return currentQuestion >= 8
  },
-//array that contains the questions and their assosciated answers, answer choices, and options
+//array that contains the questions and their assosciated answers, answer choices, and options. Also runs answerQustion askQuestion functions
   houses: [
     {
       name: "Gryffindor",
